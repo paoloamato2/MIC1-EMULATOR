1941f59424c838acd759e921c099580f921285b9
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-15 19:01:18 +0200

    Add files via upload

--- codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Interprete/FacadeInterprete.cs
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Interprete/FacadeInterprete.cs
@@ -2,12 +2,41 @@
 {
     public class FacadeInterprete
     {
+        private static readonly object Object = new object();
+        private static volatile FacadeInterprete _fi;
         private UnitàControllo _uc;
+
+        private FacadeInterprete()
+        {
+        }
+
+        public static FacadeInterprete GetInstance()
+        {
+            if (_fi != null) return _fi;
+            lock (Object)
+            {
+                if (_fi == null) _fi = new FacadeInterprete();
+            }
+
+            return _fi;
+        }
 
         public bool AvviaCiclo()
         {
             _uc = UnitàControllo.GetInstance();
             while (true) _uc.EseguiCiclo();
+        }
+
+        public void SetStepByStep(bool p0)
+        {
+            _uc = UnitàControllo.GetInstance();
+            _uc.Stepbystep = p0;
+        }
+
+        public void Reset()
+        {
+            _uc = UnitàControllo.GetInstance();
+            _uc.ResetFlag = true;
         }
     }
 }
