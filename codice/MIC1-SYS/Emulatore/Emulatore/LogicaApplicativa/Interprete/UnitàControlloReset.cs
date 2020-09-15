5fb917267cc2e401f4e43a00fe6f7988d4ea81ef
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-15 19:04:03 +0200

    UnitàControlloReset.cs

--- codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Interprete/UnitàControlloReset.cs
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Interprete/UnitàControlloReset.cs
@@ -1,4 +1,5 @@
-﻿using MIC1_SYS.Emulatore.LogicaApplicativa.Stato;
+﻿using System.Diagnostics;
+using MIC1_SYS.Emulatore.LogicaApplicativa.Stato;
 
 namespace MIC1_SYS.Emulatore.LogicaApplicativa.Interprete
 {
@@ -10,7 +11,7 @@ namespace MIC1_SYS.Emulatore.LogicaApplicativa.Interprete
 
         protected internal UnitàControlloReset()
         {
-            _fs = new FacadeStato();
+            _fs = FacadeStato.GetInstance();
         }
 
         protected override void ChangeState(UnitàControllo uc, UnitàControlloState newState)
@@ -20,16 +21,24 @@ namespace MIC1_SYS.Emulatore.LogicaApplicativa.Interprete
 
         public override void EseguiCiclo()
         {
+            DebugInfo();
             _uc = UnitàControllo.GetInstance();
             _uc.Mir = "000000001000000000000000000000001001";
             _uc.NFf = 0;
             _uc.ZFf = 0;
             _uc.ResetDone = true;
             _uc.ResetFlag = false;
+            _uc.Halt = false;
+            _uc.Termina = false;
             _fs.Reset();
 
 
             ChangeState(_uc, GetInstance("Fetch"));
+        }
+
+        private static void DebugInfo()
+        {
+            Debug.WriteLine("L'esecuzione è nello stato di reset");
         }
     }
 }
