ca89290de7c927f8cfcc36d19c8f911a5b5e3c2a
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-15 19:03:38 +0200

    UnitàControlloHalt.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Interprete/UnitàControlloHalt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MIC1_SYS.Emulatore.LogicaApplicativa.Interprete
+{
+    public class UnitàControlloHalt : UnitàControlloState
+    {
+        public static readonly object Object = new object();
+        private UnitàControllo _uc;
+
+        protected override void ChangeState(UnitàControllo uc, UnitàControlloState newState)
+        {
+            uc.SetState(newState);
+        }
+
+        [Obsolete]
+        public override void EseguiCiclo()
+        {
+            _uc = UnitàControllo.GetInstance();
+
+            Debug.WriteLine("L'esecuzione è in stato di halt");
+
+
+            if (_uc.Termina)
+                // _uc.Termina = false;
+                return;
+
+            if (_uc.Stepbystep) Thread.CurrentThread.Suspend();
+
+           
+
+            if (_uc.ResetFlag)
+            {
+                ChangeState(_uc, GetInstance("Reset"));
+                return;
+            }
+
+
+            ChangeState(_uc, GetInstance("Halt"));
+        }
+    }
+}
