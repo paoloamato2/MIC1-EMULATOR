47d060eafdfbd2ef1ee52227473fed8037a21af3
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:36:39 +0200

    ShiftRegisterStato.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/ShiftRegisterStato.cs
@@ -0,0 +1,38 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
+{
+    public class ShiftRegisterStato
+    {
+        private static readonly object Object = new object();
+        private static volatile ShiftRegisterStato _srs;
+        private readonly ShiftRegister _sr;
+
+        public ShiftRegisterStato()
+        {
+            _sr = ShiftRegister.GetInstance();
+            Dato = _sr.Dato;
+            Operation = _sr.Operation;
+        }
+
+        public string Dato { get; set; }
+
+        public string Operation { get; set; }
+
+
+        public static ShiftRegisterStato GetInstance()
+        {
+            if (_srs != null) return _srs;
+            lock (Object)
+            {
+                if (_srs == null) _srs = new ShiftRegisterStato();
+            }
+
+            return _srs;
+        }
+
+        public void AggiornaStato()
+        {
+            Dato = _sr.Dato;
+            Operation = _sr.Operation;
+        }
+    }
+}
