accbfb284e66bcbdf9eb5a82a527dd304cd960ba
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:37:55 +0200

    Stato.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/Stato.cs
@@ -0,0 +1,43 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
+{
+    public class Stato
+    {
+        private static readonly object Object = new object();
+        private static volatile Stato _stato;
+
+        public Stato()
+        {
+            StatoAggiornato = true;
+            Cms = MemoriaControlloStato.GetInstance();
+            Mcs = MemoriaCentraleStato.GetInstance();
+            Uos = UnitàOperativaStato.GetInstance();
+        }
+
+        public bool StatoAggiornato { get; set; }
+
+        public MemoriaControlloStato Cms { get; set; }
+
+        public MemoriaCentraleStato Mcs { get; set; }
+
+        public UnitàOperativaStato Uos { get; set; }
+
+        public static Stato GetInstance()
+        {
+            if (_stato != null) return _stato;
+            lock (Object)
+            {
+                if (_stato == null) _stato = new Stato();
+            }
+
+            return _stato;
+        }
+
+        public void AggiornaStato()
+        {
+            Cms.AggiornaStato();
+            Mcs.AggiornaStato();
+            Uos.AggiornaStato();
+            StatoAggiornato = true;
+        }
+    }
+}
