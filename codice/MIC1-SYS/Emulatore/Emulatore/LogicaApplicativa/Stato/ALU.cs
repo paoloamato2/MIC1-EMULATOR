01b64899e8c47b7177643212bb591c7c8ff9305b
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:22:56 +0200

    ALU.cs

--- codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/ALU.cs
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/ALU.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
 {
@@ -7,6 +10,16 @@ namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
         private static readonly object Object = new object();
         private static volatile ALU _alu;
         private ShiftRegister _sr;
+
+        public ALU()
+        {
+            Operation = "000000";
+            OperandA = "00000000000000000000000000000000";
+            OperandB = "00000000000000000000000000000000";
+            NFlag = false;
+            ZFlag = false;
+            Risultato = "00000000000000000000000000000000";
+        }
 
         public string Operation { get; set; }
 
@@ -34,11 +47,15 @@ namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
         public void execute_op()
         {
             _sr = ShiftRegister.GetInstance();
+            // f0 e f1 stabiliscono l'operazione che l'ALU deve eseguire
             var f0 = Operation[0];
             var f1 = Operation[1];
+            // enA e enB abilitano rispettivamente i due ingressi A e B dell'ALU
             var enA = Operation[2];
             var enB = Operation[3];
+            // invA inverte l'operando A se  abilitato
             var invA = Operation[4];
+            // inc effettua un incremento unitario se abilitato
             var inc = Operation[5];
 
             string tResult;
@@ -58,16 +75,16 @@ namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
             var fn = new string(chars);
             switch (fn)
             {
-                case "00":
+                case "00": //AND 
                     tResult = Utility.ToBin(Convert.ToInt32(tOperandAInv, 2) & Convert.ToInt32(tOperandB, 2), 32);
                     break;
-                case "01":
+                case "01": //OR
                     tResult = Utility.ToBin(Convert.ToInt32(tOperandAInv, 2) | Convert.ToInt32(tOperandB, 2), 32);
                     break;
-                case "10":
+                case "10": 
                     tResult = Utility.ToBin(~Convert.ToInt32(tOperandB, 2), 32);
                     break;
-                case "11":
+                case "11": //SOMMA ALGEBRICA
                 {
                     var tSum = Convert.ToString(tUSum, 2);
                     tResult = tSum;
@@ -81,10 +98,24 @@ namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
 
             Risultato = Utility.ToBin(Convert.ToInt32(tResult, 2), 32);
 
-            _sr.Dato = Risultato;
+            _sr.Dato = Risultato; //scrittura risultato nel registro a scorrimento
 
-            NFlag = Risultato[0] == '1';
-            ZFlag = Risultato == "00000000000000000000000000000000";
+            NFlag = Risultato[0] == '1'; //aggiornamento negative flag
+            ZFlag = Risultato == "00000000000000000000000000000000"; //aggiornamento zero flag
+
+            DebugInfo();
+        }
+
+        private void DebugInfo()
+        {
+            Debug.WriteLine("-----------");
+            Debug.WriteLine("Operazione ALU: " + Operation);
+            Debug.WriteLine("Operando A: " + OperandA);
+            Debug.WriteLine("Operando B " + OperandB);
+            Debug.WriteLine("Risultato: " + Risultato);
+            Debug.WriteLine("Negative Flag: " + NFlag);
+            Debug.WriteLine("Zero flag:  " + ZFlag);
+            Debug.WriteLine("-----------");
         }
     }
 }
