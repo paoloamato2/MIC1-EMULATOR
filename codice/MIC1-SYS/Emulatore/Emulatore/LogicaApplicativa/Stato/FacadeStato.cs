907a53533b22540e05429568d12f2c55437dc926
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:31:14 +0200

    FacadeStato.cs

--- codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/FacadeStato.cs
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/FacadeStato.cs
@@ -2,39 +2,71 @@
 {
     public class FacadeStato
     {
+        private static readonly object Object = new object();
+        private static volatile FacadeStato _fs;
         private readonly MemoriaControllo _mc;
         private readonly UnitàOperativa _ou;
-        private MemoriaCentrale _ram;
+        private readonly MemoriaCentrale _ram;
+        private readonly Stato _stato;
 
-        public FacadeStato()
+        private FacadeStato()
         {
             _ou = UnitàOperativa.GetInstance();
             _mc = MemoriaControllo.GetInstance();
             _ram = MemoriaCentrale.GetInstance();
+            _stato = Stato.GetInstance();
+        }
+
+        public static FacadeStato GetInstance()
+        {
+            if (_fs != null) return _fs;
+            lock (Object)
+            {
+                if (_fs == null) _fs = new FacadeStato();
+            }
+
+            return _fs;
         }
 
         public void Reset()
         {
+            //_stato.StatoAggiornato = false;
             _ou.Reset();
         }
 
         public string Fetch(string indirizzo)
         {
+           // _stato.StatoAggiornato = false;
             return _mc.LeggiMicroIstruzione(indirizzo);
         }
 
         public bool Execute(string opcode)
         {
+            //_stato.StatoAggiornato = false;
             _ou.Execute(opcode);
             return true;
         }
 
-        public void CaricaProgramma()
+        public void CaricaProgramma(string[] data)
         {
+            //_stato.StatoAggiornato = false;
+            _ram.CaricaProgramma(data);
         }
 
-        public void CaricaMicroProgramma()
+        public void CaricaMicroProgramma(string[] data)
         {
+           // _stato.StatoAggiornato = false;
+            _mc.CaricaMicroProgramma(data);
+        }
+
+        public Stato get_Stato()
+        {
+            return _stato;
+        }
+
+        public void RinnovaStato()
+        {
+            _stato.AggiornaStato();
         }
 
 
@@ -46,6 +78,11 @@
         public int[] get_ALUflag()
         {
             return _ou.get_ALUflag();
+        }
+
+        public void ClearStatoAggiornato()
+        {
+            _stato.StatoAggiornato = false;
         }
     }
 }
