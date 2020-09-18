1cdc3858a7468ea97e14e450ade102224fbcf5d1
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:56:16 +0200

    MicroProgramma.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/MicroProgramma.cs
@@ -0,0 +1,19 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa
+{
+    public class MicroProgramma
+    {
+        public MicroProgramma(int id, string nome, string[] data)
+        {
+            Id = id;
+            Nome = nome;
+            Data = data;
+        }
+
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string[] Data { get; set; }
+    }
+}
