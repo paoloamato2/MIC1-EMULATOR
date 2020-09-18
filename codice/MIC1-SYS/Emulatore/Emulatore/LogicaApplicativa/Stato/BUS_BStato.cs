1215f86e57e735d8e8f4042fcea5910a451f1306
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:25:04 +0200

    BUS_BStato.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/BUS_BStato.cs
@@ -0,0 +1,38 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
+{
+    public class BusBStato
+    {
+        private static readonly object Object = new object();
+        private static volatile BusBStato _bbs;
+        private readonly BusB _busB;
+
+
+        public BusBStato()
+        {
+            _busB = BusB.GetInstance();
+            Operation = _busB.Operation;
+            Dato = _busB.Dato;
+        }
+
+        public string Operation { get; set; }
+
+        public string Dato { get; set; }
+
+        public static BusBStato GetInstance()
+        {
+            if (_bbs != null) return _bbs;
+            lock (Object)
+            {
+                if (_bbs == null) _bbs = new BusBStato();
+            }
+
+            return _bbs;
+        }
+
+        public void AggiornaStato()
+        {
+            Operation = _busB.Operation;
+            Dato = _busB.Dato;
+        }
+    }
+}
