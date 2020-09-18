d32c80a6982f3d7ea1ce8b5f48552ea12e3548de
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:55:55 +0200

    Programma.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Programma.cs
@@ -0,0 +1,19 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa
+{
+    public class Programma
+    {
+        public Programma(int id, string nome, string[] data)
+        {
+            Id = id;
+            Nome = nome;
+            Data = data;
+        }
+
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string[] Data { get; set; }
+    }
+}
