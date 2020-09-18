a93e639e3717b164b99075841210dd28e33eb0df
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:33:59 +0200

    MemoriaControlloStato.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/MemoriaControlloStato.cs
@@ -0,0 +1,33 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
+{
+    public class MemoriaControlloStato
+    {
+        private static readonly object Object = new object();
+        private static volatile MemoriaControlloStato _mcs;
+        private readonly MemoriaControllo _mc;
+
+        public MemoriaControlloStato()
+        {
+            _mc = MemoriaControllo.GetInstance();
+            Data = _mc.Data;
+        }
+
+        public string[] Data { get; set; }
+
+        public static MemoriaControlloStato GetInstance()
+        {
+            if (_mcs != null) return _mcs;
+            lock (Object)
+            {
+                if (_mcs == null) _mcs = new MemoriaControlloStato();
+            }
+
+            return _mcs;
+        }
+
+        public void AggiornaStato()
+        {
+            Data = _mc.Data;
+        }
+    }
+}
