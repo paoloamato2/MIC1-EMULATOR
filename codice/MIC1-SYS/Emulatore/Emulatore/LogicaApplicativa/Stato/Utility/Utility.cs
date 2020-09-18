f9d3ea654c76b987d5296514bb36e239b49c62ec
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:22:27 +0200

    Utility.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/Utility/Utility.cs
@@ -0,0 +1,11 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
+{
+    public class Utility
+    {
+        //metodo che converte un valore intero decimale in una stringa binaria con fissato numero di bit
+        public static string ToBin(int value, int len)
+        {
+            return (len > 1 ? ToBin(value >> 1, len - 1) : null) + "01"[value & 1];
+        }
+    }
+}
