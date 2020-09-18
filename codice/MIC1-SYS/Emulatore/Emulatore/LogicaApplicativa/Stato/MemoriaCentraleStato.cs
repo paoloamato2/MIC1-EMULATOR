7518fc71e8f407adb19f84cae91d6f94d7b80d4c
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-18 18:32:07 +0200

    MemoriaCentraleStato.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/LogicaApplicativa/Stato/MemoriaCentraleStato.cs
@@ -0,0 +1,34 @@
+﻿namespace MIC1_SYS.Emulatore.LogicaApplicativa.Stato
+{
+    public class MemoriaCentraleStato
+    {
+        private static readonly object Object = new object();
+        private static volatile MemoriaCentraleStato _mcs;
+        private readonly MemoriaCentrale _ram;
+
+        public MemoriaCentraleStato()
+        {
+            _ram = MemoriaCentrale.GetInstance();
+            Data = _ram.Data;
+        }
+
+        public string[] Data { get; set; }
+
+        public static MemoriaCentraleStato GetInstance()
+        {
+            if (_mcs != null) return _mcs;
+            lock (Object)
+            {
+                if (_mcs == null) _mcs = new MemoriaCentraleStato();
+            }
+
+            return _mcs;
+        }
+
+
+        public void AggiornaStato()
+        {
+            Data = _ram.Data;
+        }
+    }
+}
