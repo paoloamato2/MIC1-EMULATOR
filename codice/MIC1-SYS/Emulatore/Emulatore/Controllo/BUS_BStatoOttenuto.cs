8b7860b9113bd0ae27c42e5a374f5d1d7e76b00c
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-20 20:37:39 +0200

    BUS_BStatoOttenuto.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/Controllo/BUS_BStatoOttenuto.cs
@@ -0,0 +1,31 @@
+﻿namespace MIC1_SYS.Emulatore.Controllo
+{
+    public class BusBStatoOttenuto
+    {
+        private static readonly object Object = new object();
+        private static volatile BusBStatoOttenuto _bbso;
+
+
+        public BusBStatoOttenuto()
+        {
+            Operation = "0000";
+            Dato = "000000000000000000000000000000";
+        }
+
+
+        public string Operation { get; set; }
+
+        public string Dato { get; set; }
+
+        public static BusBStatoOttenuto GetInstance()
+        {
+            if (_bbso != null) return _bbso;
+            lock (Object)
+            {
+                if (_bbso == null) _bbso = new BusBStatoOttenuto();
+            }
+
+            return _bbso;
+        }
+    }
+}
