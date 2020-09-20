2ef581542148faca1240d15d0039bd759ce31574
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-20 20:39:07 +0200

    RegistriStatoOttenuto.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/Controllo/RegistriStatoOttenuto.cs
@@ -0,0 +1,54 @@
+﻿namespace MIC1_SYS.Emulatore.Controllo
+{
+    public class RegistriStatoOttenuto
+    {
+        private static readonly object Object = new object();
+        private static volatile RegistriStatoOttenuto _rso;
+
+        public RegistriStatoOttenuto()
+        {
+            Mar = "00000000000000000000000000000000";
+            Mdr = "00000000000000000000000000000000";
+            Pc = "00000000000000000000000000000000";
+            Mbr = "00000000000000000000000000000000";
+            Sp = "00000000000000000000000000000000";
+            Lv = "00000000000000000000000000000000";
+            Cpp = "00000000000000000000000000000000";
+            Tos = "00000000000000000000000000000000";
+            Opc = "00000000000000000000000000000000";
+            H = "00000000000000000000000000000000";
+        }
+
+
+        public string Mar { get; set; }
+
+        public string Mdr { get; set; }
+
+        public string Pc { get; set; }
+
+        public string Mbr { get; set; }
+
+        public string Sp { get; set; }
+
+        public string Lv { get; set; }
+
+        public string Cpp { get; set; }
+
+        public string Tos { get; set; }
+
+        public string Opc { get; set; }
+
+        public string H { get; set; }
+
+        public static RegistriStatoOttenuto GetInstance()
+        {
+            if (_rso != null) return _rso;
+            lock (Object)
+            {
+                if (_rso == null) _rso = new RegistriStatoOttenuto();
+            }
+
+            return _rso;
+        }
+    }
+}
