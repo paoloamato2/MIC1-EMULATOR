fc7e2195e0132e6aa8fe8b51df91dc325cf0414a
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-20 20:38:38 +0200

    MicroProgrammaOttenuto.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/Controllo/MicroProgrammaOttenuto.cs
@@ -0,0 +1,15 @@
+﻿namespace MIC1_SYS.Emulatore.Controllo
+{
+    public class MicroProgrammaOttenuto
+    {
+        public MicroProgrammaOttenuto(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
