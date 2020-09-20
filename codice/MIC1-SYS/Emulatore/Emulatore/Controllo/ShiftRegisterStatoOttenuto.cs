22d843eb49eb4a9c12da559e4afe01573ef7836f
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-20 20:39:23 +0200

    ShiftRegisterStatoOttenuto.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/Controllo/ShiftRegisterStatoOttenuto.cs
@@ -0,0 +1,30 @@
+﻿namespace MIC1_SYS.Emulatore.Controllo
+{
+    public class ShiftRegisterStatoOttenuto
+    {
+        private static readonly object Object = new object();
+        private static volatile ShiftRegisterStatoOttenuto _srso;
+
+        public ShiftRegisterStatoOttenuto()
+        {
+            Dato = "00000000000000000000000000000000";
+            Operation = "00";
+        }
+
+        public string Dato { get; set; }
+
+        public string Operation { get; set; }
+
+
+        public static ShiftRegisterStatoOttenuto GetInstance()
+        {
+            if (_srso != null) return _srso;
+            lock (Object)
+            {
+                if (_srso == null) _srso = new ShiftRegisterStatoOttenuto();
+            }
+
+            return _srso;
+        }
+    }
+}
