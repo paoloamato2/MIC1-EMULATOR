b2d7e5b4ae4ed3b0df53d5505c708e00cecd4dda
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-20 20:37:06 +0200

    AluStatoOttenuto.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/Controllo/AluStatoOttenuto.cs
@@ -0,0 +1,41 @@
+﻿namespace MIC1_SYS.Emulatore.Controllo
+{
+    public class AluStatoOttenuto
+    {
+        private static readonly object Object = new object();
+        private static volatile AluStatoOttenuto _aso;
+
+        public AluStatoOttenuto()
+        {
+            Operation = "000000";
+            OperandA = "00000000000000000000000000000000";
+            OperandB = "00000000000000000000000000000000";
+            Risultato = "00000000000000000000000000000000";
+            NFlag = false;
+            ZFlag = false;
+        }
+
+        public string Operation { get; set; }
+
+        public string OperandA { get; set; }
+
+        public string OperandB { get; set; }
+
+        public string Risultato { get; set; }
+
+        public bool NFlag { get; set; }
+
+        public bool ZFlag { get; set; }
+
+        public static AluStatoOttenuto GetInstance()
+        {
+            if (_aso != null) return _aso;
+            lock (Object)
+            {
+                if (_aso == null) _aso = new AluStatoOttenuto();
+            }
+
+            return _aso;
+        }
+    }
+}
