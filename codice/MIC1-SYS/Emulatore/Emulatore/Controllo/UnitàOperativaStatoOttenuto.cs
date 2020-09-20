bab8f91168ee779b4ab6a5bd9dcd4bfffbc963f5
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-20 20:40:15 +0200

    UnitàOperativaStatoOttenuto.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/Controllo/UnitàOperativaStatoOttenuto.cs
@@ -0,0 +1,41 @@
+﻿namespace MIC1_SYS.Emulatore.Controllo
+{
+    public class UnitàOperativaStatoOttenuto
+    {
+        private static readonly object Object = new object();
+        private static volatile UnitàOperativaStatoOttenuto _uoso;
+
+        public UnitàOperativaStatoOttenuto()
+        {
+            Srso = ShiftRegisterStatoOttenuto.GetInstance();
+            Aso = AluStatoOttenuto.GetInstance();
+            Bcso = BusCStatoOttenuto.GetInstance();
+            Bbso = BusBStatoOttenuto.GetInstance();
+            Rso = RegistriStatoOttenuto.GetInstance();
+            Operation = "000000000000000000000000000000000000";
+        }
+
+        public string Operation { get; set; }
+
+        public ShiftRegisterStatoOttenuto Srso { get; set; }
+
+        public AluStatoOttenuto Aso { get; set; }
+
+        public BusCStatoOttenuto Bcso { get; set; }
+
+        public BusBStatoOttenuto Bbso { get; set; }
+
+        public RegistriStatoOttenuto Rso { get; set; }
+
+        public static UnitàOperativaStatoOttenuto GetInstance()
+        {
+            if (_uoso != null) return _uoso;
+            lock (Object)
+            {
+                if (_uoso == null) _uoso = new UnitàOperativaStatoOttenuto();
+            }
+
+            return _uoso;
+        }
+    }
+}
