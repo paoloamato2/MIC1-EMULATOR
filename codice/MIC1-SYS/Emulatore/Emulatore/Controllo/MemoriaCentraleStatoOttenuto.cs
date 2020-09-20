d005192ca4a924c9d3699faa9731a05a570c5cf6
Paolo Amato <69688166+paoloamato2@users.noreply.github.com>
2020-09-20 20:38:09 +0200

    MemoriaCentraleStatoOttenuto.cs

---
+++ codice/MIC1-SYS/Emulatore/Emulatore/Controllo/MemoriaCentraleStatoOttenuto.cs
@@ -0,0 +1,27 @@
+﻿namespace MIC1_SYS.Emulatore.Controllo
+{
+    public class MemoriaCentraleStatoOttenuto
+    {
+        private static readonly object Object = new object();
+        private static volatile MemoriaCentraleStatoOttenuto _mcso;
+
+
+        public MemoriaCentraleStatoOttenuto()
+        {
+            Data = new string[512];
+        }
+
+        public string[] Data { get; set; }
+
+        public static MemoriaCentraleStatoOttenuto GetInstance()
+        {
+            if (_mcso != null) return _mcso;
+            lock (Object)
+            {
+                if (_mcso == null) _mcso = new MemoriaCentraleStatoOttenuto();
+            }
+
+            return _mcso;
+        }
+    }
+}
